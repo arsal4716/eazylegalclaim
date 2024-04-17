79a299859e9330d317d4196552109b51e688fc2d
Arsalan Ali <arsalanali124000@gmail.com>
2024-04-18 01:32:51 +0500

    fix some issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
